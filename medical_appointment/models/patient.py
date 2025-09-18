218611f2c66d425fde2c75cc6b700bf8f1d1661c
semakarayel <skarayel35@gmail.com>
2025-09-18 22:12:04 +0300

    Import date, add gender and active fields to patient.py

--- medical_appointment/models/patient.py
+++ medical_appointment/models/patient.py
@@ -1,18 +1,22 @@
-from odoo import api, fields, models
+from odoo import fields, models, api
+from datetime import date
+
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
 
-    # fields
+    # fields + 'gender', 'active',
     patient_id = fields.Char('Patient ID', required=True)
     first_name = fields.Char('First Name', required=True)
     last_name = fields.Char('Last Name', required=True)
     full_name = fields.Char(string='Full Name', compute='_compute_full_name', store=True)
     date_of_birth = fields.Date(string='Date of Birth')
     age = fields.Integer(string='Age', compute='_compute_age', readonly=True)
+    gender = fields.Selection([('male','Male'), ('female','Female')], string='Gender')
     address = fields.Text('Address')
     phone = fields.Char(string='Phone')
     email = fields.Char(string='Email')
     national_id_no = fields.Char(string='National ID No')
+    active = fields.Boolean('Active', default=True)
 
