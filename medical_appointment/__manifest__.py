f4ed357a2c4065af1ab4ab296b4a5017f6bed511
semakarayel <skarayel35@gmail.com>
2025-09-22 19:10:28 +0300

    Create appointment.py

--- medical_appointment/__manifest__.py
+++ medical_appointment/__manifest__.py
@@ -11,7 +11,8 @@
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
-        'views/male_patient_view.xml'
+        'views/male_patient_view.xml',
+        'views/appointment_view.xml'
     ],
     'demo': [],
     'installable': True,
