1902802d86eda6eb3278568dc858dc412fbdb1a5
semakarayel <skarayel35@gmail.com>
2025-09-26 12:34:37 +0300

    add fields and button action functions in appointment.py

--- medical_appointment/models/appointment.py
+++ medical_appointment/models/appointment.py
@@ -6,16 +6,60 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'ref'
 
-
     # fields
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection(related='patient_id.gender')
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
-    ref = fields.Char(string='Reference')
-
-
+    ref = fields.Char(string='Refere nce', help="Reference from patient record.")
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string='Priority')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', ' Cancelled')], default='draft', string='Status', required=True)
+    doctor_id = fields.Many2one('res.users', string='Doctor', tracking=True)
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
+
+    def action_test(self):
+        print("Button Clicked !")
+        return {
+            'effect': {
+                'fadeout': 'fast',
+                'message': '',
+                'type': 'rainbow_man',
+            }
+        }
+
+    # Control statusbar using buttons
+    def action_in_consultation(self):
+        for rec in self:
+            rec.state = 'in_consultation'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
+
+    def action_draft(self):
+        for rec in self:
+            rec.state = 'draft'
+
+class AppointmentPharmacyLines(models.Model):
+    _name = 'appointment.pharmacy.lines'
+    _description = 'Appointment Pharmacy Lines'
+
+    product_id = fields.Many2one('product.product')
+    price_unit = fields.Float(string='Price')
+    qty = fields.Integer(string='Quantity')
