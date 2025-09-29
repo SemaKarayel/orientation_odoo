5097fdcfa844df1a94083cbbca89be34f712883f
semakarayel <skarayel35@gmail.com>
2025-09-29 15:46:17 +0300

    Created patient_tag model and related views, menus and access rights

--- medical_appointment/__manifest__.py
+++ medical_appointment/__manifest__.py
@@ -5,14 +5,15 @@
     'sequence': -100,
     'summary': 'Hospital Management System',
     'description': """Hospital Management System""",
-    'depends': ['mail'],
+    'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
         'views/male_patient_view.xml',
-        'views/appointment_view.xml'
+        'views/appointment_view.xml',
+        'views/patient_tag_view.xml'
     ],
     'demo': [],
     'installable': True,
