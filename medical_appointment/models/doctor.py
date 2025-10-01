0f521c96ca30550f58fe006b57b7575a449e4330
semakarayel <skarayel35@gmail.com>
2025-10-01 15:47:46 +0300

    Created doctor.py and view

---
+++ medical_appointment/models/doctor.py
@@ -0,0 +1,44 @@
+from odoo import models, fields, api
+from datetime import date
+
+
+class Doctor(models.Model):
+    _name = 'hospital.doctor'
+    _description = 'Hospital Doctor'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'full_name'
+
+    # fields
+    first_name = fields.Char('First Name', required=True, tracking=True)
+    last_name = fields.Char('Last Name', required=True, tracking=True)
+    full_name = fields.Char(string='Full Name', compute='_compute_full_name', store=True, tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
+    age = fields.Integer(string='Age', compute='_compute_age', readonly=True)
+    phone = fields.Char(string='Phone')
+    email = fields.Char(string='Email')
+    department_id = fields.Many2one('hospital.department', string='Department')
+    shift_start = fields.Float(string='Shift Start')
+    shift_end = fields.Float(string='Shift End')
+
+    # Compute full name
+    @api.depends('first_name', 'last_name')
+    def _compute_full_name(self):
+        for rec in self:
+            rec.full_name = f"{rec.first_name} {rec.last_name}"
+
+    # compute age
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            if rec.date_of_birth:
+                today = date.today()
+                rec.age = today.year - rec.date_of_birth.year - (
+                        (today.month, today.day) < (rec.date_of_birth.month, rec.date_of_birth.day)
+                )
+            else:
+                rec.age = 0
+
+    # Unique email constraint
+    _sql_constraints = [
+        ('email_unique', 'unique(email)', 'Email must be unique.')
+    ]
