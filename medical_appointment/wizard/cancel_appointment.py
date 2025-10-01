d4b0bf29ffee691a1689810094708e595e30db3b
semakarayel <skarayel35@gmail.com>
2025-10-01 16:24:34 +0300

    typo correction in cancel_appointment.py

--- medical_appointment/wizard/cancel_appointment.py
+++ medical_appointment/wizard/cancel_appointment.py
@@ -5,4 +5,4 @@ class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
     _description = 'Cancel Appointment Wizard'
 
-    appointment_id = fields.Many2one('hospotal.appointment', string='Appointment')
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
