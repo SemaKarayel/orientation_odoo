52b23c5a06ef587bba6b5639fae3ba63124e6803
semakarayel <skarayel35@gmail.com>
2025-10-06 11:38:49 +0300

    Load data from xml file for patient_tag

--- medical_appointment/__manifest__.py
+++ medical_appointment/__manifest__.py
@@ -8,6 +8,7 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
         'wizard/cancel_appointment_view.xml',
         'views/create_appointment_wizard_view.xml',
         'views/menu.xml',
