4638f3fc754a4e208618fd1f8b94e05733195b8b
semakarayel <skarayel35@gmail.com>
2025-10-06 11:11:55 +0300

    Return wizard from button in cancellation

--- medical_appointment/models/appointment.py
+++ medical_appointment/models/appointment.py
@@ -65,8 +65,8 @@ class HospitalAppointment(models.Model):
             rec.state = 'done'
 
     def action_cancel(self):
-        for rec in self:
-            rec.state = 'cancel'
+        action = self.env.ref("medical_appointment.action_cancel_appointment").read()[0]
+        return action
 
     def action_draft(self):
         for rec in self:
